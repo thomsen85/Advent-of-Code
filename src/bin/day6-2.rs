6b89612067881e096a55ac8cb3c3c4a9638de0b8
Thomas <thomassvendal@gmail.com>
2022-12-06 13:35:14 +0100

    Day 6 Complete

---
+++ src/bin/day6-2.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input: Vec<char> = aoc2022_rust::utils::lines_from_file("inputs/day6.txt").first().unwrap().to_owned().chars().collect();
+    let mut p = 0; 
+    println!("{:?}", input.len());
+    while p + 13 < input.len() {
+        let mut m = true;
+        for i in 0..14 {
+            for j in 0..14 {
+                if j != i && input[p+i] == input[p+j] {
+                    m = false;
+                }
+            }
+        }
+
+        if m {
+            break
+        }
+        p+=1
+    }
+
+    println!("Match at: {}", p + 14 );
+}
