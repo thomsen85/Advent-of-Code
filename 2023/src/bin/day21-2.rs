6a710634982d4bb5b61393ddeb3b839bbaaed069
Thomas <thomassvendal@gmail.com>
2023-12-28 15:58:03 +0100

    feat(2023)/Day-21-2-Need-to-speed-up

--- 2023/src/bin/day21-2.rs
+++ 2023/src/bin/day21-2.rs
@@ -1,14 +1,22 @@
-use std::{collections::HashSet, process::exit};
+use rstest::rstest;
+use std::{collections::HashSet, f64};
 
 use common::utils;
 use glam::IVec2;
-use indicatif::ProgressIterator;
+use ndarray::{arr1, Array2};
+use ndarray_linalg::Inverse;
 
 fn main() {
     dbg!(solve(include_str!("../../inputs/day21.txt"), 26501365));
+
+    // 621289980339486 Too High
+    // 621289922886178 Too High
+    // 621289922885837 : ?
+    // 10 , 3 : 621289922898471: ?
 }
 
 fn solve(input: &str, steps: usize) -> String {
+    println!("Solving for {} steps", steps);
     let map = utils::string_to_grid(input);
 
     let start = map
@@ -23,21 +31,39 @@ fn solve(input: &str, steps: usize) -> String {
         .unwrap()
         .0;
 
-    const MOVES: [IVec2; 4] = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
-    let rows = map.len();
-    let columns = map[0].len();
-    dbg!(rows, columns, steps);
-    let mut visited: HashSet<IVec2> = HashSet::from([start]);
+    // Assert that it is square
+    {
+        let rows = map.len();
+        let columns = map[0].len();
+        assert_eq!(rows, columns);
+    }
+    let size = map.len();
+    println!("Map is of size {}x{}", size, size);
 
-    for _step in 0..steps {
+    let rem = steps % size;
+    let start_i = 10;
+    let sample_points = 3;
+
+    println!(
+        "Collecting {} sample points after {} gardens",
+        sample_points, start_i
+    );
+
+    let mut visited: HashSet<IVec2> = HashSet::from([start]);
+    let mut y_p = vec![];
+    let mut x_p = vec![];
+    let mut samples = 0;
+
+    const MOVES: [IVec2; 4] = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
+    for step in 0..steps {
         let visited_c = visited.clone();
         visited.clear();
 
         for p in visited_c {
             for mov in MOVES {
                 let new_p = p + mov;
-                if map[new_p.x.rem_euclid(rows as i32) as usize]
-                    [new_p.y.rem_euclid(columns as i32) as usize]
+                if map[new_p.x.rem_euclid(size as i32) as usize]
+                    [new_p.y.rem_euclid(size as i32) as usize]
                     == '#'
                 {
                     continue;
@@ -47,24 +73,71 @@ fn solve(input: &str, steps: usize) -> String {
             }
         }
 
-        if _step % 5 == 0 {
-            println!(
-                "{}\t{}",
-                _step,
-                (visited.len() as f64).sqrt() / _step as f64
-            );
+        if step >= start_i * size && (step + 1) % size == rem {
+            y_p.push(visited.len() as f64);
+            x_p.push(1. + step as f64);
+            samples += 1;
+            println!("Collected {}. sample", samples);
+        }
+
+        if samples >= sample_points {
+            break;
         }
     }
 
-    visited.len().to_string()
+    if x_p.len() < 3 {
+        return visited.len().to_string();
+    }
+    // https://en.wikipedia.org/wiki/Polynomial_regression
+    // 1 x_n (x^2)_n
+    // n = x_p.len()
+    // 4, 2 array
+
+    let n = x_p.len();
+    let m: usize = 3;
+    let mut x: Array2<f64> = Array2::ones((n, m));
+    for (i, val) in x_p.iter().enumerate() {
+        x[[i, 1]] = *val;
+        x[[i, 2]] = val.powi(2);
+    }
+    println!("x:\n{}", &x);
+
+    // 1, 4
+    let y = arr1(&y_p);
+    let y = y.to_shape((n, 1)).unwrap().to_owned();
+    println!("y:\n{}", &y);
+
+    let b = x
+        .t()
+        .to_owned()
+        .dot(&x.clone())
+        .inv()
+        .unwrap()
+        .dot(&x.clone().t().dot(&y));
+    let e = y - x.dot(&b);
+
+    println!("e:\n{}", &e);
+
+    dbg!(steps);
+    let ans = dbg!(arr1(&[1., steps as f64, steps.pow(2) as f64]).dot(&b))[0];
+
+    (ans.round() as usize).to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_1() {
+    #[rstest]
+    #[case(6, "16")]
+    #[case(10, "50")]
+    #[case(50, "1594")]
+    #[case(100, "6536")]
+    #[case(500, "167004")]
+    #[case(1000, "668697")]
+    #[case(5000, "16733044")]
+
+    fn test_1(#[case] steps: usize, #[case] count: &str) {
         let ti = "...........
 .....###.#.
 .###.##..#.
@@ -76,36 +149,6 @@ mod tests {
 .##.#.####.
 .##..##.##.
 ...........";
-        assert_eq!(solve(ti, 6), "16".to_string());
-    }
-    #[test]
-    fn test_2() {
-        let ti = "...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........";
-        assert_eq!(solve(ti, 10), "50".to_string());
-    }
-    #[test]
-    fn test_3() {
-        let ti = "...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........";
-        assert_eq!(solve(ti, 500), "167004".to_string());
+        assert_eq!(solve(ti, steps), count.to_string());
     }
 }
