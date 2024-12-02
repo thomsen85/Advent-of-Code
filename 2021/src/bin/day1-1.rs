b6f907fa6e104adea2d7c2668f7157b0e5efe295
Thomas Svendal <thomassvendal@gmail.com>
2024-12-02 10:53:04 +0100

    Day 1 2021 Done

---
+++ 2021/src/bin/day1-1.rs
@@ -0,0 +1,35 @@
+use common::strings::string_to_t_vec;
+use itertools::Itertools;
+
+fn main() {
+    dbg!(solve(include_str!("../../inputs/day1.txt")));
+}
+
+fn solve(input: &str) -> String {
+    string_to_t_vec::<i32>(input)
+        .iter()
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let ti = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        assert_eq!(solve(ti), "7".to_string());
+    }
+}
